6f2a9f243d0eae60f3aed70fe4dfe876fbac7fea
Muhafiq <muhafiq18@gmail.com>
2022-12-31 16:13:02 +0700

    First push

---
+++ script.js
@@ -0,0 +1,154 @@
+let data;
+const savedData = JSON.parse(localStorage.getItem('rahasia'));
+
+if (Array.isArray(savedData)) {
+    data = savedData;
+} else {
+    data = [{
+        name: 'afiq',
+        age: 18,
+        profession: 'Mahasiswa',
+        id: 'id1'
+    }, {
+        name: 'muh',
+        age: 23,
+        profession: 'Web Developer',
+        id: 'id2'
+    }, {
+        name: 'mamun',
+        age: 34,
+        profession: 'Graphic Designer',
+        id: 'id3'
+    }];
+}
+
+read();
+
+function createData(addName, addAge, addProfession) {
+    const id = '' + new Date().getTime();
+    data.push({
+        name: addName,
+        age: addAge,
+        profession: addProfession,
+        id: id
+    });
+    saveData();
+}
+
+function removeData(idToDelete) {
+    data = data.filter(function (param) {
+        if (param.id === idToDelete) {
+            return false;
+        } else {
+            return true;
+        }
+    });
+    saveData();
+}
+
+function saveData() {
+    localStorage.setItem('rahasia', JSON.stringify(data));
+}
+
+// add data
+document.getElementById('add-button').addEventListener('click', function (event) {
+    event.preventDefault();
+    const addName = document.getElementById('input-name').value;
+    const addAge = document.getElementById('input-age').value;
+    const addProfession = document.getElementById('input-profession').value;
+
+    if (addName === '' || addAge === '' || addProfession === '') {
+        return;
+    }
+    createData(addName, addAge, addProfession);
+    read();
+
+    document.getElementById('input-name').value = '';
+    document.getElementById('input-age').value = '';
+    document.getElementById('input-profession').value = '';
+});
+
+// delete data
+function deleteData(event) {
+    const deleteButton = event.target;
+    const idToDelete = deleteButton.id;
+
+    removeData(idToDelete);
+    read();
+}
+// update data
+function updateData(event) {
+    const idToUpdate = event.target.id;
+
+    for (let i = 0; i < data.length; i++) {
+        if (idToUpdate === data[i].id) {
+            document.getElementById('input-name').value = data[i].name;
+            document.getElementById('input-age').value = data[i].age;
+            document.getElementById('input-profession').value = data[i].profession;
+
+            const addBtn = document.getElementById('add-button');
+
+            const updateBtn = document.createElement('button');
+            updateBtn.innerText = 'Update Data';
+
+            document.getElementById('button').appendChild(updateBtn);
+            document.getElementById('button').removeChild(addBtn);
+
+            updateBtn.addEventListener('click', function (event) {
+                event.preventDefault();
+                data[i].name = document.getElementById('input-name').value;
+                data[i].age = document.getElementById('input-age').value;
+                data[i].profession = document.getElementById('input-profession').value;
+
+                updateBtn.remove();
+                document.getElementById('button').appendChild(addBtn);
+                read();
+                saveData();
+
+                document.getElementById('input-name').value = '';
+                document.getElementById('input-age').value = '';
+                document.getElementById('input-profession').value = '';
+
+            });
+            break;
+        }
+    }
+}
+
+// read data
+function read() {
+    document.getElementById('container').innerHTML = '';
+    const head = document.createElement('tr');
+    head.insertCell(0).innerHTML = 'Nama';
+    head.insertCell(1).innerHTML = 'Umur';
+    head.insertCell(2).innerHTML = 'Pekerjaan';
+    head.insertCell(3).innerHTML = 'Ubah';
+    head.insertCell(4).innerHTML = 'Hapus';
+    head.style = 'font-weight: bold; text-align: center;';
+
+    document.getElementById('container').appendChild(head);
+
+    data.forEach(function (param) {
+        const container = document.createElement('tr');
+        // delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.onclick = deleteData;
+        deleteButton.id = param.id;
+
+        // update button
+        const updateButton = document.createElement('button');
+        updateButton.innerText = 'Update';
+        updateButton.onclick = updateData;
+        updateButton.id = param.id;
+        updateButton.className = 'update';
+
+        container.insertCell(0).innerHTML = param.name;
+        container.insertCell(1).innerHTML = param.age;
+        container.insertCell(2).innerHTML = param.profession;
+        container.insertCell(3).appendChild(updateButton);
+        container.insertCell(4).appendChild(deleteButton);
+
+        document.getElementById('container').appendChild(container);
+    });
+}
